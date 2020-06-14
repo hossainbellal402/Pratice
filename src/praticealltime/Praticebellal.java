81b8b2f084f9af0afedfd5effbcee5c266577f49
hossainbellal402 <66582075+hossainbellal402@users.noreply.github.com>
2020-06-14 15:46:54 -0400

    add the feature

--- src/praticealltime/Praticebellal.java
+++ src/praticealltime/Praticebellal.java
@@ -1,9 +1,16 @@
 package praticealltime;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Praticebellal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		
+	System.setProperty( "webdriver.chrome.driver", "C:\\Users\\bella\\Desktop\\Github jar\\drivers\\chromedriver.exe");	
+		WebDriver driver=new ChromeDriver();
+		 driver.get("https://www.google.com/");
 
 	}
 
