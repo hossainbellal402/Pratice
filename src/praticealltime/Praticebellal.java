389c2b1015653c1bfbfb9b686b7f3b13f23fecdd
hossainbellal402 <66582075+hossainbellal402@users.noreply.github.com>
2020-06-14 17:51:00 -0400

    adding

--- src/praticealltime/Praticebellal.java
+++ src/praticealltime/Praticebellal.java
@@ -2,12 +2,13 @@ package praticealltime;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Praticebellal {
 
-	public static void main(String[] args) {
+@Test 
 	
-		
+	public void openBrowser() {	
 	System.setProperty( "webdriver.chrome.driver", "C:\\Users\\bella\\Desktop\\Github jar\\drivers\\chromedriver.exe");	
 		WebDriver driver=new ChromeDriver();
 		 driver.get("https://www.google.com/");
